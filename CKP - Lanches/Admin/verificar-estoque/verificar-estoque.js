996568b0cd91696eb532929b69884ca24c99011e
PCantarin <pedrocantarin89@gmail.com>
2024-09-05 14:41:03 -0300

    Add files via upload

---
+++ CKP - Lanches/Admin/verificar-estoque/verificar-estoque.js
@@ -0,0 +1,40 @@
+// script.js
+
+// Obtém o modal
+var modal = document.getElementById("janelaAddProduto");
+
+// Obtém o botão que abre o modal
+var btn = document.getElementById("addProduto");
+
+// Obtém o elemento <span> que fecha o modal
+var span = document.getElementsByClassName("botao-cancelar")[0];
+
+// Quando o usuário clicar no botão, abre o modal
+btn.onclick = function () {
+    modal.style.display = "flex";
+}
+
+// Quando o usuário clicar no botão, fecha o modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+
+//gerar PDF - Lista de compras
+
+document.getElementById('listaCompras').addEventListener('click', async () => {
+    const iframe = document.getElementById('pdfContent');
+    const content = iframe.contentDocument || iframe.contentWindow.document;
+
+    const options = {
+        margin: 0.5,
+        filename: 'lista_de_compras.pdf',
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    html2pdf().from(content.body).set(options).save();
+});
+
+
+//gerar PDF - Relatório de movimentações

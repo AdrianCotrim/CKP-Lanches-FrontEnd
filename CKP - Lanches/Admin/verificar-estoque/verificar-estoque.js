626bd24a4058c7a9f0073ac7fe9f5a6b22a683f6
AdrianCotrim <adriancotrimdosreis4@gmail.com>
2024-09-06 17:34:19 -0300

    Mostra valores do insumo nos inputs do modal

--- CKP - Lanches/Admin/verificar-estoque/verificar-estoque.js
+++ CKP - Lanches/Admin/verificar-estoque/verificar-estoque.js
@@ -30,6 +30,19 @@ var modalAltInsumo = document.getElementById('alterarInsumo')
 estoque.addEventListener('click', (event) => {
     if(event.target.nodeName == 'TD'){
         modalAltInsumo.style.display = 'flex'
+
+        var insumo = event.target.parentNode
+        let celulas = Array.from(insumo.children)
+
+        var nome = celulas[0].textContent;
+        var descricao = celulas[2].textContent;
+        var qtdMinima = celulas[3].textContent;
+        var quantidade = celulas[4].textContent;
+            
+        document.getElementById('nomeAlterar').value = nome;
+        document.getElementById('descricaoAlterar').value = descricao;
+        document.getElementById('QtdMinimaAlterar').value = qtdMinima;
+        document.getElementById('quantidadeAlterar').value = quantidade;
     }   
 })
 

651cfb948ef18ec0f61bb32afe15ba3565185325
AdrianCotrim <adriancotrimdosreis4@gmail.com>
2024-09-06 14:32:56 -0300

    ModalAdicionarInsumo

--- CKP - Lanches/Admin/verificar-estoque/verificar-estoque.js
+++ CKP - Lanches/Admin/verificar-estoque/verificar-estoque.js
@@ -1,27 +1,30 @@
-// script.js
-
-// Obtém o modal
-var modal = document.getElementById("janelaAddProduto");
+// Obtém o modais
+var modalAddInsumo = document.getElementById("adicionarInsumo");
+var modalAltInsumo = document.getElementById("alterarInsumo")
 
 // Obtém o botão que abre o modal
 var btn = document.getElementById("addProduto");
 
 // Obtém o elemento <span> que fecha o modal
-var span = document.getElementsByClassName("botao-cancelar")[0];
+var span = document.getElementById("cancelar");
+var span2 = document.getElementById("confirmar");
 
 // Quando o usuário clicar no botão, abre o modal
 btn.onclick = function () {
-    modal.style.display = "flex";
+    modalAddInsumo.style.display = "flex";
 }
 
 // Quando o usuário clicar no botão, fecha o modal
 span.onclick = function () {
-    modal.style.display = "none";
+    modalAddInsumo.style.display = "none";
+}
+
+span2.onclick = function () {
+    modalAddInsumo.style.display = "none";
 }
 
 // Alterar Produto
 var estoque = document.getElementById('estoque')
-var modalAlterarProd = document.getElementById("alterarProduto")
 
 estoque.addEventListener('click', (event) => {
     if(event.target.nodeName == 'TD'){

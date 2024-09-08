51cd527ebf3ca7eac054222847f85977fac483bf
PCantarin <pedrocantarin89@gmail.com>
2024-09-08 14:07:55 -0300

    listadecompras-pdf

--- CKP - Lanches/Admin/verificar-estoque/verificar-estoque.js
+++ CKP - Lanches/Admin/verificar-estoque/verificar-estoque.js
@@ -24,35 +24,44 @@ var estoque = document.getElementById('estoque')
 var modalAlterarProd = document.getElementById("alterarProduto")
 
 estoque.addEventListener('click', (event) => {
-    if(event.target.nodeName == 'TD'){
-        modalAlterarProd.style.display  = 'flex'
+    if (event.target.nodeName == 'TD') {
+        modalAlterarProd.style.display = 'flex'
     }
 })
 
 modalAlterarProd.addEventListener('click', (event) => {
-    if(event.target.textContent == 'Concluir'){
+    if (event.target.textContent == 'Concluir') {
         modalAlterarProd.style.display = 'none'
     }
-    if(event.target.textContent == 'Cancelar'){
+    if (event.target.textContent == 'Cancelar') {
         modalAlterarProd.style.display = 'none'
     }
 })
 
 // Gerar PDF - Lista de compras
 
-document.getElementById('listaCompras').addEventListener('click', async () => {
-    const iframe = document.getElementById('pdfContent');
-    const content = iframe.contentDocument || iframe.contentWindow.document;
+function imprimirTabela() {
+    // Cria uma nova janela para impressão
+    const janelaImpressao = window.open('', '', 'height=1000,width=1000');
 
-    const options = {
-        margin: 0.5,
-        filename: 'lista_de_compras.pdf',
-        html2canvas: { scale: 2 },
-        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-    };
+    // Obtém o conteúdo da tabela
+    const conteudoTabela = document.getElementById('lista-de-compras').outerHTML;
+    
+    // Adiciona o HTML básico para a nova janela
+    janelaImpressao.document.write('<html><head><link rel="stylesheet" type="text/css" href="../style.css"><link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"><title>Imprimir Tabela</title>');
+    janelaImpressao.document.write('<style>body{font-family: "Fredoka", sans-serif;} table { width: 100%; border: none} th, td {padding: 8px; text-align: left; } th { background-color: var(--cinza-bg); } img{width: 100%}</style>');
+    janelaImpressao.document.write('</head><body >');
+    janelaImpressao.document.write('<img src="/Imagens/cabecalho-lista-de-compras.png">')
+    janelaImpressao.document.write(conteudoTabela);
+    janelaImpressao.document.write('</body></html>');
 
-    html2pdf().from(content.body).set(options).save();
-});
+    // Fecha o documento e aguarda a renderização
+    janelaImpressao.document.close();
+    janelaImpressao.focus();
+
+    // Executa o comando de impressão
+    janelaImpressao.print();
+}
 
 
 // Gerar PDF - Relatório de movimentações

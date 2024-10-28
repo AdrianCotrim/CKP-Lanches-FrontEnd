ce46aec1ce39126336240c2e3391ab5934b4314b
AdrianCotrim <adriancotrimdosreis4@gmail.com>
2024-10-28 17:26:38 -0300

    Deletar pedido

--- Admin/registrar-pedidos/alterar-pedidos.js
+++ Admin/registrar-pedidos/alterar-pedidos.js
@@ -1,24 +1,25 @@
 // Modal
 var modalAddPedido = document.getElementById("janelaRegisPedido")
 const modalAlterarInfoPedido = document.getElementById("alterarInfoPedido")
+var pedidoAlterar = null;
 
 // Exibe modal na tela
 pedidos.addEventListener('click', (event) => {  
     if(event.target.nodeName == 'TD'){
         modalAlterarInfoPedido.style.display = 'flex'
+        pedidoAlterar = event.target.parentNode;
+        
 
-        var pedido = event.target.parentNode
-        let celulas = Array.from(pedido.children)
-
+        
+        /* let celulas = Array.from(pedido.children)
         var nome = "";
         // var id = celulas[1].textContent;
         var tipoPedido = celulas[3].textContent;
         var endereco = "";
-          
         document.getElementById('nomeAlterar').value = nome;
         // document.getElementById('id').value = id;
         document.getElementById('tipoPedidoAlterar').value = tipoPedido;
-        document.getElementById('enderecoAlterar').value = endereco;
+        document.getElementById('enderecoAlterar').value = endereco; */
     }
 
     if(event.target.nodeName == 'DIV'){
@@ -28,9 +29,28 @@ pedidos.addEventListener('click', (event) => {
 
 modalAlterarInfoPedido.addEventListener("click", function(event) {
   if(event.target.textContent == 'Concluir'){
-      modalAlterarInfoPedido.style.display = "none"
+    modalAlterarInfoPedido.style.display = "none"
   }
+
+  if(event.target.textContent == 'Deletar'){
+    event.preventDefault();
+    const produtoId = pedidoAlterar.children[1].textContent;
+    fetch(`http://localhost:8080/pedidos/${produtoId}`, {
+      headers: {
+          'Authorization': `Bearer ${token}`,
+          'Accept': 'application/json',
+          'Content-Type':'application/json'
+      },
+      method: 'DELETE',
+    })
+    .then(response =>{
+        console.log(response)
+        window.location.reload()
+    })
+    .catch(erro => console.log(erro))
+  }
+
   if(event.target.textContent == 'Cancelar'){
-      modalAlterarInfoPedido.style.display = "none"
+    modalAlterarInfoPedido.style.display = "none"
   }
 })
